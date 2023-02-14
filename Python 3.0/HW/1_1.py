cf46fd70d10f22f93b3525555ccf7360df913307
Artikras <112582866+Artikras@users.noreply.github.com>
2023-02-14 19:31:51 +0300

    Add files via upload

---
+++ Python 3.0/HW/1_1.py
@@ -0,0 +1,10 @@
+# 1. Напишите программу, которая принимает на вход цифру,
+#  обозначающую день недели, и проверяет, является ли этот день выходным.
+
+num = int(input())
+if 5 < num < 8:
+    print("Weekand") 
+elif 0 < num < 6:
+    print("Workday")
+else:
+    print("It's not a day on the week")

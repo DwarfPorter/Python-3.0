ef7afe9c43fb5504490af8ca14ecb703494e7206
Vladimir Morozov <mrvlmor@gmail.com>
2023-04-20 06:40:31 +0300

    Delete Промежуточная контрольная 2 работа по блоку  directory

--- Промежуточная контрольная 2 работа по блоку /Client/Program.java
+++
@@ -1,9 +0,0 @@
-package Client;
-
-import UI.App;
-
-public class Program {
-    public static void main(String[] args) {
-        App.buttonClick();
-    }
-}
--- Промежуточная контрольная 2 работа по блоку /Configs/Config.java
+++
@@ -1,6 +0,0 @@
-package Configs;
-
-public class Config {
-    public static String pathDb = "Configs/prizes";
-    public static String pathResult = "Configs/drawResults";
-}
